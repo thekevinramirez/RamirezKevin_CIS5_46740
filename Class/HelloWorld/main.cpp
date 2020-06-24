ad52d16cecbd8f8459e3f63f00906542b8cf049f
BuildTools <kramirez101@student.rccd.edu>
2020-06-23 20:27:12 -0700

    testing

---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Kevin Ramirez
+ * Purpose: Creating first program - Hello World program
+ * Created on June 23, 2020, 8:12 PM
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+
+//Execution of Code Begins Here
+
+int main(int argc, char** argv) {
+    //Set the random number seed here
+
+    //Declare all variables for this function
+
+    //Initialize all known variables
+
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+
+    //Display the Inputs/Outputs
+    cout << "Hello World" << endl;
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    return 0;
+}
